17bffce2cd657904faaea1c909f87b5272268aeb
ajbond <olishmollie@gmail.com>
2018-05-24 17:07:49 -0500

    Improve TwoSummands problem description

--- src/main/java/TwoSummands.java
+++ src/main/java/TwoSummands.java
@@ -1,10 +1,15 @@
 import java.util.HashSet;
 import java.util.HashMap;
 
-// Given an array of integers and a sum, find the two elements in the array that add up to
-// the given sum.
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// Example
+// Given nums = [2, 7, 11, 15], target = 9,
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
+
 public class TwoSummands {
-    // Time and space complexity O(n)
     public static HashSet<Integer> solution(int a[], int sum) {
         HashSet<Integer> result = new HashSet<>();
         if (a == null) { return result; }
